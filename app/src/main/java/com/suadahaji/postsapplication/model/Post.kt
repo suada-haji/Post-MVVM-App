7223f9319a624863806ef5db5c82af24fc74eb8f
Suada Haji <suadahaji@gmail.com>
2020-12-02 19:18:07 +0300

    Add Post model

---
+++ app/src/main/java/com/suadahaji/postsapplication/model/Post.kt
@@ -0,0 +1,6 @@
+package com.suadahaji.postsapplication.model
+
+data class Post(var userId : Int,
+                var id: Int,
+                var title: String,
+                var body: String)
