f16bfa66996959a51edce8cbf08e0bc18a7ba262
Suada Haji <suadahaji@gmail.com>
2020-12-02 20:43:50 +0300

    Add error layout

--- app/src/main/java/com/suadahaji/postsapplication/MainActivity.kt
+++ app/src/main/java/com/suadahaji/postsapplication/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.suadahaji.postsapplication
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.suadahaji.postsapplication.ui.PostListAdapter
 import com.suadahaji.postsapplication.ui.PostListViewModel
+import com.suadahaji.postsapplication.utils.NetworkState
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.error_layout.view.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: PostListViewModel
@@ -24,5 +27,25 @@ class MainActivity : AppCompatActivity() {
             )
             postRecycler.adapter = PostListAdapter(posts)
         })
+
+        viewModel.status.observe(this, { status ->
+            when(status) {
+                NetworkState.SUCCESS -> {
+                    progressBar.visibility = View.GONE
+                    errorLayout.visibility = View.GONE
+                }
+                NetworkState.LOADING -> {
+                    progressBar.visibility = View.VISIBLE
+                    errorLayout.visibility = View.GONE
+                }
+                NetworkState.error(status.msg) -> {
+                    progressBar.visibility = View.GONE
+                    errorLayout.visibility = View.VISIBLE
+                    errorLayout.errorText.text = getString(R.string.no_internet)
+                }
+            }
+        })
+
+        errorLayout.retryBtn.setOnClickListener { viewModel.getPosts() }
     }
 }
