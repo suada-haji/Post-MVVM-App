876387f4c55cc2f28a90d6b0c15235a786e0e97a
Suada Haji <suadahaji@gmail.com>
2020-12-02 19:29:20 +0300

    Annotate Post Data class for Moshi

--- app/src/main/java/com/suadahaji/postsapplication/model/Post.kt
+++ app/src/main/java/com/suadahaji/postsapplication/model/Post.kt
@@ -1,5 +1,8 @@
 package com.suadahaji.postsapplication.model
 
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
 data class Post(var userId : Int,
                 var id: Int,
                 var title: String,
